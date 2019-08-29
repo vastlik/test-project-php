43b73df90268ebbbdd10906f7683345b41b2be4d
Martin Vastl <martin.vastl2@gmail.com>
2019-08-29 10:42:26 +0200

    Homepage form frontend and backend validation.

--- create.php
+++ create.php
@@ -1,15 +1,83 @@
 <?php
-
+/** @var App $app */
 $app = require "./core/app.php";
 
-// Create new instance of user
-$user = new User($app->db);
-// Insert it to database with POST data
-$user->insert(array(
-	'name' => $_POST['name'],
-	'email' => $_POST['email'],
-	'city' => $_POST['city']
-));
 
-// Redirect back to index
-header('Location: index.php');
+/**
+ * @var string $name
+ * @return true if name is valid, otherwise false
+ */
+function isNameValid($name) {
+    return !empty($name);
+}
+
+/**
+ * @var string $email
+ * @return true if name is valid, otherwise false
+ */
+function isEmailValid($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+}
+
+/**
+ * @var string $city
+ * @return true if city is valid, otherwise false
+ */
+function isCityValid($city) {
+    return !empty($city);
+}
+
+
+if($_SERVER["REQUEST_METHOD"] != "POST") {
+    header('Location: index.php');
+}
+
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$city = trim($_POST['city']);
+
+$flashMessages = [];
+$valid = true;
+
+if( !isNameValid($name) ) {
+    $valid = false;
+    $flashMessages[] = [
+            'type' => 'danger',
+            'text' => "Invalid username.",
+    ];
+}
+
+if( !isEmailValid($email) ) {
+    $valid = false;
+    $flashMessages[] = [
+            'type' => 'danger',
+            'text' => "Invalid email.",
+    ];
+}
+
+if( !isCityValid($city) ) {
+    $valid = false;
+    $flashMessages[] = [
+            'type' => 'danger',
+            'text' => "Invalid city.",
+    ];
+}
+
+if($valid) {
+    $flashMessages[] = [
+            'type' => 'success',
+            'text' => "User was successfully added.",
+    ];
+    $user = new User($app->db);
+    $user->insert(array(
+        'name' => $name,
+        'email' => $email,
+        'city' => $city,
+    ));
+}
+
+$users = User::find($app->db, '*');
+$app->renderView('index', array(
+    'users' => $users,
+    'flashMessages' => $flashMessages,
+));
--- views/index.php
+++ views/index.php
@@ -1,5 +1,13 @@
 <h1>PHP Test Application</h1>
 
+<? if(isset($flashMessages)) { ?>
+    <? foreach ($flashMessages as $message) { ?>
+        <div class="alert alert-<?= $message['type'] ?>" role="alert">
+            <?= $message['text'] ?>
+        </div>
+    <?php } ?>
+<?php } ?>
+
 <table class="table table-striped">
 	<thead>
 		<tr>
@@ -20,24 +28,29 @@
 </table>				
 
 <form method="post" action="create.php">
-    <div class="form-group row">
-        <label for="name" class="col-sm-1 col-form-label">Name:</label>
+    <div class="form-group required row">
+        <label for="name" class="col-sm-1 col-form-label control-label">Name:</label>
         <div class="col-sm-4">
-            <input name="name" class="form-control" input="text" id="name"/>
+            <input name="name" class="form-control" type="text" id="name" required/>
         </div>
     </div>
 
-    <div class="form-group row">
-        <label for="email" class="col-sm-1 col-form-label">E-mail:</label>
+    <div class="form-group required row">
+        <label for="email" class="col-sm-1 col-form-label control-label">E-mail:</label>
         <div class="col-sm-4">
-            <input name="email" class="form-control" input="text" id="email"/>
+            <input name="email" class="form-control" type="email" id="email" required/>
         </div>
     </div>
 
-    <div class="form-group row">
-        <label for="city" class="col-sm-1 col-form-label">City:</label>
+    <div class="form-group required row">
+        <label for="city" class="col-sm-1 col-form-label control-label">City:</label>
         <div class="col-sm-4">
-            <input name="city" class="form-control" input="text" id="city"/>
+            <input name="city" class="form-control" type="text" id="city" required/>
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-4">
+            Fields with * are required.
         </div>
     </div>
 	<button class="btn btn-primary">Create new row</button>
